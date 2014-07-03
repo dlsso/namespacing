15cf6a8fc3a6485132bc520ddb49c8bb548a129a
dlsso <supertonesfan@hotmail.com>
2014-07-03 14:18:38 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// // GLOBALS
+// var name = 'RefactorU'
+// var getData = function () {
+// 	return ['Robby', 'Alice', 'Gene', 'Frederick', 'Eleanor'];
+
+// };
+
+
+// Technique #1: namespacing
+var App = {
+//	var name = 'RefactorU'
+	var name : 'RefactorU'
+	
+	var getData = function () {
+		return ['Robby', 'Alice', 'Gene', 'Frederick', 'Eleanor'];
+
+	};
+}
